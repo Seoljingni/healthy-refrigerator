702c7d6f5e2171f5be0bb4006d46f99a19dab262
JiHaneggg <gjwlgod1234@naver.com>
2023-12-03 23:40:41 +0900

    ai.c

--- JI/Term/Term/ai.c
+++ JI/Term/Term/ai.c
@@ -1,108 +1,303 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-// 음식 구조체 정의
+
+#define MAX_FOOD_ITEMS 100
+#define MAX_NAME_LENGTH 50
+
+
+int total_fooditem = 0;
+int total_selectitem = 0;
+int first_total_fooditem = 0;
+
+
 typedef struct {
-    char name[20]; // 음식 이름
-    int weight; // 음식 무게
-    int calorie; // 음식 칼로리
-    float cal_per_weight; // 칼로리/무게
-    int selected; // 선택 여부
-    int expiry; // 유통기한
-    int carb; // 탄수화물
-    int protein; // 단백질
-    int fat; // 지방
-    float priority; // 가중치
-} Food;
+    char name[MAX_NAME_LENGTH];
+    int weight;
+    int expiration;
+    float calories;
+    int carbo;
+    int protein;
+    int fat;
+    double value;
+} FoodItem;
 
-// 가중치에 따른 정렬 함수
-int compare_priority(const void* a, const void* b) {
-    Food* foodA = (Food*)a;
-    Food* foodB = (Food*)b;
-    return foodA->priority < foodB->priority ? 1 : -1;
+
+typedef struct {
+    FoodItem* choose_food;
+    int intake_weight;
+    float intake_calories;
+} SelectItem;
+
+
+
+void fooditem_swap(FoodItem* a, FoodItem* b)
+{
+    FoodItem temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void get_food(Food* fridge, int* fridge_capacity, int* total_calorie, int* max_weight_per_turn) {
-    for (int i = 0; i < 10; i++) {
-        // 냉장고 용량이 음식 무게보다 적거나, 한 번에 꺼낼 수 있는 용량이 100을 초과하면 건너뜀
-        if (*fridge_capacity < fridge[i].weight || fridge[i].weight > *max_weight_per_turn) continue;
 
-        fridge[i].selected = 1; // 음식 선택
-        *fridge_capacity -= fridge[i].weight; // 냉장고 용량 감소
-        *total_calorie += fridge[i].calorie; // 총 칼로리 증가
 
-        // 한 번에 꺼낼 수 있는 최대 용량 갱신
-        *max_weight_per_turn -= fridge[i].weight;
+void load_food_items(FILE* file, FoodItem food_items[], int* num_items) {
+    *num_items = 0;
+    while (fscanf(file, "%99[^,],%d,%d,%f,%d,%d,%d\n",
+        food_items[*num_items].name, &food_items[*num_items].weight, &food_items[*num_items].expiration,
+        &food_items[*num_items].calories, &food_items[*num_items].carbo,
+        &food_items[*num_items].protein, &food_items[*num_items].fat) == 7) {
+        (*num_items)++;
+        total_fooditem++;
+        first_total_fooditem++;
     }
 }
+
+
+
+void set_value(FoodItem* food_items, int carbo_prior, int protein_prior, int fat_prior) {
+
+    for (int i = 0; i < total_fooditem; i++) {
+
+        if (carbo_prior == 1 && protein_prior == 2 && fat_prior == 3)
+            food_items[i].value = (food_items[i].carbo * 1.0) + (food_items[i].protein * 0.7) + (food_items[i].fat * 0.4) - ((roundf(food_items[i].calories / (float)food_items[i].weight * 100)) / 100) * 10;
+
+        else if (carbo_prior == 1 && protein_prior == 3 && fat_prior == 2)
+            food_items[i].value = (food_items[i].carbo * 1.0) + (food_items[i].protein * 0.4) + (food_items[i].fat * 0.7) - ((roundf(food_items[i].calories / (float)food_items[i].weight * 100)) / 100) * 10;
+
+        else if (carbo_prior == 2 && protein_prior == 1 && fat_prior == 3)
+            food_items[i].value = (food_items[i].carbo * 0.7) + (food_items[i].protein * 1.0) + (food_items[i].fat * 0.4) - ((roundf(food_items[i].calories / (float)food_items[i].weight * 100)) / 100) * 10;
+
+        else if (carbo_prior == 2 && protein_prior == 3 && fat_prior == 1)
+            food_items[i].value = (food_items[i].carbo * 0.4) + (food_items[i].protein * 1.0) + (food_items[i].fat * 0.7) - ((roundf(food_items[i].calories / (float)food_items[i].weight * 100)) / 100) * 10;
+
+        else if (carbo_prior == 3 && protein_prior == 1 && fat_prior == 2)
+            food_items[i].value = (food_items[i].carbo * 0.7) + (food_items[i].protein * 0.4) + (food_items[i].fat * 1.0) - ((roundf(food_items[i].calories / (float)food_items[i].weight * 100)) / 100) * 10;
+
+        else if (carbo_prior == 3 && protein_prior == 2 && fat_prior == 1)
+            food_items[i].value = (food_items[i].carbo * 0.4) + (food_items[i].protein * 0.7) + (food_items[i].fat * 1.0) - ((roundf(food_items[i].calories / (float)food_items[i].weight * 100)) / 100) * 10;
+    }
+
+}
+
+
+
+void sort_item(FoodItem* food_items, FoodItem* sort_list[]) {
+
+    int m = 0;
+
+    for (int i = 0; i < first_total_fooditem; i++)
+        if (strcmp(food_items[i].name, "NULL") != 0) {
+            sort_list[m] = &food_items[i];
+            m++;
+        }
+
+    total_fooditem = m;
+
+    for (int j = 0; j < m; j++) {
+        int temp = j;
+        for (int k = j + 1; k < m; k++) {
+            if (sort_list[temp]->value <= sort_list[k]->value)
+                temp = k;
+        }
+        fooditem_swap(sort_list[temp], sort_list[j]);
+    }
+
+}
+
+
+
+void select(FoodItem* sort_list[], SelectItem select_item[]) {
+
+    int remain_weight = 100;
+    int t = 0;
+
+    for (int i = 0; i < total_fooditem; i++) {
+
+        if (sort_list[i]->expiration <= 1) {
+            select_item[t].choose_food = sort_list[i];
+            if (sort_list[i]->weight <= remain_weight) {
+                select_item[t].intake_weight = sort_list[i]->weight;
+                select_item[t].intake_calories = sort_list[i]->calories;
+                sort_list[i]->weight = 0;
+            }
+            else {
+                select_item[t].intake_weight = remain_weight;
+                select_item[t].intake_calories = ((roundf(sort_list[i]->calories / (float)sort_list[i]->weight * 100)) / 100) * remain_weight;
+                sort_list[i]->weight = sort_list[i]->weight - select_item[t].intake_weight;
+                sort_list[i]->calories = sort_list[i]->calories - select_item[t].intake_calories;
+            }
+            remain_weight = remain_weight - select_item[t].intake_weight;
+            t++;
+            if (remain_weight == 0) break;
+        }
+    }
+
+    for (int i = 0; i < total_fooditem; i++) {
+
+        if (sort_list[i]->expiration > 1) {
+            if (sort_list[i]->expiration <= 2 && sort_list[i]->weight > 100)
+                continue;
+            else {
+                select_item[t].choose_food = sort_list[i];
+                if (sort_list[i]->weight <= remain_weight) {
+                    select_item[t].intake_weight = sort_list[i]->weight;
+                    select_item[t].intake_calories = sort_list[i]->calories;
+                    sort_list[i]->weight = 0;
+                }
+                else {
+                    select_item[t].intake_weight = remain_weight;
+                    select_item[t].intake_calories = ((roundf(sort_list[i]->calories / (float)sort_list[i]->weight * 100)) / 100) * remain_weight;
+                    sort_list[i]->weight = sort_list[i]->weight - select_item[t].intake_weight;
+                    sort_list[i]->calories = sort_list[i]->calories - select_item[t].intake_calories;
+                }
+                remain_weight = remain_weight - select_item[t].intake_weight;
+                t++;
+                if (remain_weight == 0) break;
+            }
+        }
+    }
+
+    total_selectitem = t;
+
+}
+
+
+
+void print_selectitem(SelectItem select_item[]) {
+    printf("\033[0;33mSelect:\033[0m \n");
+    for (int i = 0; i < total_selectitem; i++) {
+        if (i == total_selectitem - 1) {
+            printf("%s %d(weight)\n", select_item[i].choose_food->name, select_item[i].intake_weight);
+        }
+        else {
+            printf("%s %d(weight) \n", select_item[i].choose_food->name, select_item[i].intake_weight);
+        }
+    }
+}
+
+
+
+void print_calories(SelectItem select_item[]) {
+
+    float total_calories = 0;
+
+    for (int i = 0; i < total_selectitem; i++) {
+        total_calories = total_calories + select_item[i].intake_calories;
+    }
+    printf("\033[0;33mTotal calories:\033[0m\n%.2f\n", total_calories);
+}
+
+
+
+void update_item(FoodItem food_items[]) {
+
+    for (int i = 0; i < first_total_fooditem; i++) {
+        food_items[i].expiration--;
+        if (food_items[i].expiration == 0 || food_items[i].weight == 0 || (food_items[i].expiration <= 2 && food_items[i].weight > 100))
+            strcpy(food_items[i].name, "NULL");
+    }
+}
+
+
+int print_leftitem(FoodItem food_items[]) {
+    int flag = 0;
+    int count = 0;
+
+    printf("\033[0;33mLeftovers:\033[0m \n");
+
+    for (int i = 0; i < first_total_fooditem; i++) {
+        if (strcmp(food_items[i].name, "NULL") != 0) {
+            printf("%s\n", food_items[i].name);
+            flag++;
+        }
+    }
+
+    if (flag == 0) {
+        printf("NONE");
+        return 1;
+    }
+    return 0;
+}
+
+
+
 
 int main() {
-    Food fridge[10]; // 냉장고에 있는 음식들
-    int fridge_capacity = 1000; // 냉장고 용량
-    int total_calorie = 0; // 총 칼로리
 
-    // 음식 정보 초기화
-    Food food1 = { "chicken breast", 30, 160, 0, 0, 10, 0, 30, 2, 0 };
-    Food food2 = { "buritto", 50, 350, 0, 0, 3, 45, 15, 10, 0 };
-    Food food3 = { "yogurt", 80, 100, 0, 0, 4, 15, 10, 5, 0 };
-    Food food4 = { "meetball spagetti", 100, 480, 0, 0, 5, 60, 20, 15, 0 };
-    Food food5 = { "bread", 40, 300, 0, 0, 2, 55, 10, 5, 0 };
-    Food food6 = { "egg", 18, 140, 0, 0, 7, 1, 12, 10, 0 };
-    Food food7 = { "Kimchi Fried Rice", 150, 650, 0, 0, 2, 70, 15, 12, 0 };
-    Food food8 = { "pork cutlet", 50, 425, 0, 0, 15, 15, 65, 20, 0 };
-    Food food9 = { "pizza", 110, 510, 0, 0, 5, 0, 60, 5, 35 };
-    Food food10 = { "tteokbokki", 150, 550, 0, 0, 11, 0, 75, 2, 23 };
+    FILE* input_file = fopen("C:\\Users\\gjwlg\\healthy-refrigerator\\JI\\Term\\input.txt", "r");
 
-    fridge[0] = food1;
-    fridge[1] = food2;
-    fridge[2] = food3;
-    fridge[3] = food4;
-    fridge[4] = food5;
-    fridge[5] = food6;
-    fridge[6] = food7;
-    fridge[7] = food8;
-    fridge[8] = food9;
-    fridge[9] = food10;
-
-    float arr[100];
-
-    // 각 음식의 용량당 칼로리와 탄단지 가중치 계산
-    for (int i = 0; i < 10; i++) {
-        fridge[i].cal_per_weight = (float)fridge[i].calorie / fridge[i].weight;
-        arr[i] = (fridge[i].carb * 1.0 + fridge[i].protein * 0.7 + fridge[i].fat * 0.4);
-        fridge[i].priority = (fridge[i].carb * 1.0 + fridge[i].protein * 0.7 + fridge[i].fat * 0.4) - (fridge[i].cal_per_weight * 10);
+    if (input_file == NULL) {
+        fprintf(stderr, "\033[0;31mError opening input file.\033[0m\n");
+        return 1;
     }
 
-    printf("\n\n");
-    for (int i = 0; i < 10; i++) {
-        printf("%.2lf\n", arr[i]);
-    }
+    FoodItem food_items[MAX_FOOD_ITEMS];
+    SelectItem select_item[MAX_FOOD_ITEMS];
+    FoodItem* sort_list[MAX_FOOD_ITEMS];
 
-    // 가중치에 따라 정렬
-    qsort(fridge, 10, sizeof(Food), compare_priority);
+    int num_items;
+    int day = 1;
+    int value1, value2, value3;
 
-    // 한 번에 꺼낼 수 있는 최대 용량
-    int max_weight_per_turn = 100;
 
-    while (fridge_capacity > 0 && max_weight_per_turn > 0) {
-        get_food(fridge, &fridge_capacity, &total_calorie, &max_weight_per_turn);
-    }
+    load_food_items(input_file, food_items, &num_items);
 
-    // 선택된 음식과 총 칼로리 출력
-    for (int i = 0; i < 10; i++) {
-        if (fridge[i].selected) {
-            printf("선택된 음식: %s, 무게: %d, 칼로리: %d, 가중치: %.2f, 탄수화물: %d, 단백질: %d, 지방: %d, 용량당 칼로리: %.2f\n", fridge[i].name, fridge[i].weight, fridge[i].calorie, fridge[i].priority, fridge[i].carb, fridge[i].protein, fridge[i].fat, fridge[i].cal_per_weight);
+    printf("Place in the order of importance : 1 carbohydrates, 2 proteins, and 3 fats.\n");
+    printf("(ex: 2 1 3) -> This means that it is weighted in the order of protein, carbohydrates, and fat.\n\n");
+
+
+    while (1) {
+
+        
+        printf("\033[1mDay:%d\033[0m", day);
+        printf("\n");
+
+        scanf("%d %d %d", &value1, &value2, &value3);
+        printf("\n");
+
+
+        if (value1 == 1 && value2 == 2 && value3 == 3)
+            set_value(food_items, 1, 2, 3);
+        else if (value1 == 1 && value2 == 3 && value3 == 2)
+            set_value(food_items, 1, 3, 2);
+        else if (value1 == 2 && value2 == 1 && value3 == 3)
+            set_value(food_items, 2, 1, 3);
+        else if (value1 == 2 && value2 == 3 && value3 == 1)
+            set_value(food_items, 2, 3, 1);
+        else if (value1 == 3 && value2 == 1 && value3 == 2)
+            set_value(food_items, 3, 1, 2);
+        else if (value1 == 3 && value2 == 2 && value3 == 1)
+            set_value(food_items, 3, 2, 1);
+        else {
+            printf("\033[0;31mwrong value!\033[0m");
+            exit(1);
+        }
+
+
+        sort_item(food_items, sort_list);
+        select(sort_list, select_item);
+        print_selectitem(select_item);
+        printf("\n");
+        print_calories(select_item);
+        printf("\n");
+        update_item(food_items);
+        int flag = print_leftitem(food_items);
+        printf("\n\n");
+        day++;
+
+        printf("==========================================================\n\n");
+
+        if (flag == 1) {
+            break;
         }
     }
-    printf("총 칼로리: %d\n", total_calorie);
 
-    // 남은 음식 이름 출력
-    for (int i = 0; i < 10; i++) {
-        if (!fridge[i].selected) {
-            printf("남은 음식: %s, 가중치: %.2f, 탄수화물: %d, 단백질: %d, 지방: %d, 용량당 칼로리: %.2f\n", fridge[i].name, fridge[i].priority, fridge[i].carb, fridge[i].protein, fridge[i].fat, fridge[i].cal_per_weight);
-        }
-    }
+    printf("\033[0;36mThe refrigerator is empty!\033[0m\n");
+
+    fclose(input_file);
 
     return 0;
 }
